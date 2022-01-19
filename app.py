c7c8917b84b2a44f3bce19f94047f043b0fa29eb
MULLURUHARIKA19 <88077033+MULLURUHARIKA19@users.noreply.github.com>
2022-01-19 20:51:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import flask
+from flask import Flask, render_template
+app = flask.Flask(__name__)
+@app.route('/')
+def hello_rsk_world():
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
+   
